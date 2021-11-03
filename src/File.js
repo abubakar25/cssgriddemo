14a4fa3c2e56dd22296401cea8a06004c4db23e1
abubakar25 <92583142+abubakar25@users.noreply.github.com>
2021-11-03 03:55:25 -0700

    change 2 files

--- src/File.js
+++ src/File.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function file(props) {
-  return <div>hello this is file</div>;
+  return (
+    <div>
+      <div>how are you</div>
+      <div>hello this is file</div>
+    </div>
+  );
 }
 
 export default file;
--- src/Text.js
+++ src/Text.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Test(props) {
-  return <div>Hello this is test</div>;
+  return (
+    <div>
+      <div>Hello this is test </div>
+    </div>
+  );
 }
 
 export default Test;
